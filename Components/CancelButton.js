2b70cb7266f1e9b66b45a7a24dda551dfd5c6539
cathyfu1215 <phoenixfucong@gmail.com>
2024-08-29 12:32:28 -0700

    modified the style for save and cancel button so there are no texts but shapes

--- Components/CancelButton.js
+++ Components/CancelButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Pressable, Text} from 'react-native';
 import styleHelper from '../styleHelper';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const CancelButton = ({ onPress }) => {
   return (
     <Pressable style={styleHelper.cancelButton} onPress={onPress}>
-      <Text style={styleHelper.cancelButtonText}>Cancel</Text>
+      <MaterialIcons name="cancel" size={24} color="black" />
     </Pressable>
   );
 };
--- Components/SaveButton.js
+++ Components/SaveButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Pressable, Text} from 'react-native';
 import styleHelper from '../styleHelper';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 const SaveButton = ({ onPress }) => {
   return (
     <Pressable style={styleHelper.saveButton} onPress={onPress}>
-      <Text style={styleHelper.cancelButtonText}>Save</Text>
+     <FontAwesome name="check" size={24} color="black" />
     </Pressable>
   );
 };
